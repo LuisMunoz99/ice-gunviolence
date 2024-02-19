0a7a2e1bb4075598331b0366f580c759d004dc63
Luis <luis.munoz8@upr.edu>
2024-02-19 16:17:33 -0400

    export regdem data for geocoding

--- geocode/export/src/export_geocoding.R
+++ geocode/export/src/export_geocoding.R
@@ -1,6 +1,6 @@
 # Authors:     LM
 # Maintainers: LM
-# Date: 18-Feb-24
+# Date: 19-Feb-24
 # =========================================
 
 # Exporting cases of interest for geocoding 
@@ -11,24 +11,69 @@
 if(!require(pacman))install.packages("pacman")
 p_load(dplyr, 
        here,
-       readxl,
+       data.table,
+       openxlsx,
        stringr)
 
 
 args <- list(input = here("import/regdem/output/regdem2021-2022.csv"),
-             output = here("geocode/export/output/geocoding_minors.xlsx")
-)
+             output = here("geocode/export/output/geocoding_minors.xlsx"))
 
 
 # --- Import data --- 
-regdem <- read_excel(args$input1)
+regdem <- fread(args$input)
 
+
+
+# --- Cleaning --- 
 
 ## SEPARATE THEM IN DIFFERENT YEAR EACH SHEET
+child_fire_21 <- regdem %>% filter(year_info == 2021) %>% 
+  select(year_info,ControlNumber,Name, LastName,
+         'DeathCause_I (ID)','DeathCause_I (Desription)',
+         firearm,minors,
+         ResidencePlaceAddress1:ResidencePlaceAddressZip) %>% 
+  filter(minors == 1 & firearm == 1) %>% 
+  mutate(longitude = NULL,
+         latitute = NULL)
 
-## Export only columns of interest
 
 
-## PUT EVERYTHING TO LOWER
-# CLEAN ANY SPECIAL CASES
-# CREATE BLACK variables called - longitude and latitude
+child_fire_22  <- regdem %>% filter(year_info == 2022) %>% 
+  select(year_info,ControlNumber,Name, LastName,
+         'DeathCause_I (ID)','DeathCause_I (Desription)',
+         firearm,minors,
+         ResidencePlaceAddress1:ResidencePlaceAddressZip) %>% 
+  filter(minors == 1 & firearm == 1) %>% 
+  mutate(longitude = NULL,
+         latitute = NULL)
+
+## Adding empty variables for manual data entry 
+
+
+## Cleaning variables
+child_fire_21 <- child_fire_21 %>% 
+  mutate_all(~tolower(.)) %>%
+  mutate_all(~str_replace_all(., "\\s+", "")) %>%
+  mutate_all(~str_replace_all(., "[^a-zA-Z0-9]", ""))
+
+child_fire_22 <- child_fire_22 %>% 
+  mutate_all(~tolower(.)) %>%
+  mutate_all(~str_replace_all(., "\\s+", "")) %>%
+  mutate_all(~str_replace_all(., "[^a-zA-Z0-9]", "")) 
+
+
+## The first mutate_all(~tolower(.)), it means "apply the tolower function to each column of the data frame."
+## The second mutate_all removes extra spaces with str_replace_all using "\\s+" regex.
+## The third mutate_all removes special characters using str_replace_all with "[^a-zA-Z0-9]" regex.
+
+
+
+
+# --- Output --- 
+write.xlsx(list("2021" = child_fire_21, "2022" = child_fire_22), 
+           file = args$output, 
+           sheetNames = c("2021", "2022"))
+
+
+## DONE
