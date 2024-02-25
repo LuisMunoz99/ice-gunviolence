4a0f73216f3b415c3e7aa6b8e3dde516c053c7c4
Luis <luis.munoz8@upr.edu>
2024-02-24 23:16:18 -0400

    reorg

--- geocode/manual_rev/src/export_regdem_CF.R
+++ geocode/manual_rev/src/export_regdem_CF.R
@@ -17,7 +17,7 @@ p_load(dplyr,
 
 
 args <- list(input = here("geocode/import/output/regdem_CF.csv"),
-             output = here("geocode/export/output/geocoding_minors.xlsx"))
+             output = here("geocode/manual_rev/output/geocoding_CF.xlsx"))
 
 
 # --- Import data --- 
