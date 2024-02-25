82e72b981a83c62a3b3368644c6d0e999d1865e8
Luis <luis.munoz8@upr.edu>
2024-02-24 23:17:08 -0400

    Import PUMS DATA

--- ice_inc/import/PUMS_inc/src/pums_inc_import.R
+++ ice_inc/import/PUMS_inc/src/pums_inc_import.R
@@ -1,50 +1,57 @@
 # Authors:     LM
 # Maintainers: LM
-# Date: 21-feb-24
+# Date: 24-feb-24
 # ===========================================
 
 # Importing PUMS data for income household 
-# to establish quintiles for Puerto Rico 
+# Selecting only variables of interest
 
 # -- libs ---
 if(!require(pacman))install.packages("pacman")
 p_load(dplyr,
+       data.table,
        here,
        tidycensus,
        tidyr)
 
 # args {{{
-args <- list(output =
-               here("ice_inc/import/PUMS_inc/output/PUMS_incomeHH.csv"))
+args <- list(input = here("ice_inc/import/PUMS_inc/input/psam_h72.csv"),
+             output = here("ice_inc/import/PUMS_inc/output/pumsHIncome.csv"))
 
 data(pums_variables)
 
-
-pums_vars_2019 <- pums_variables %>% 
-  filter(year == 2019, survey == "acs1")
-
-pums_vars_2019 %>% 
+pums_variables %>% 
+  filter(year == 2019, survey == "acs1") %>% 
   distinct(var_code, var_label, data_type, level)
 
-
-pums_vars_2018 %>% 
-  distinct(var_code, var_label, data_type, level) %>% 
-  filter(level == "housing")
-
-
 # -- import ---
-PUMS_inc <- get_pums(
-  geography = "tract",
-  variables = c("B01003_001","B17001_002","B02001_002","B02001_003"), 
-  state = "PR",
-  year = 2022,
-  survey = "acs5",
-  geometry = TRUE,
-  show_call = TRUE)
 
+## PUMS_inc <- get_pums(
+##  variables = "HINCP", 
+## state = "PR",
+## year = 2022,
+## survey = "acs5")
+
+# https://github.com/walkerke/tidycensus/issues/425
+# This is an issue 
 
 # Vars from Census 
-# B01003_001 = Poblacion total
-# B17001_002 = Personas bajo el nivel de pobreza
-# B02001_002 = Personas blancas 
-# B02001_003 = Personas negras alone 
+# HINCP = Income Household 
+
+pums_inc <- fread(args$input) %>% 
+  select(RT:ACCESSINET,HINCP) %>% 
+  filter(HINCP >= 0)
+
+summary(pums_inc)
+
+## Keeping only positive values. 
+
+
+# Export the table of quintiles to a text file
+write.csv(pums_inc, args$output)
+
+
+
+
+
+
