eee5dc406649346348f359ae8fdb6da4093b5b21
Luis <luis.munoz8@upr.edu>
2024-03-10 14:33:20 -0400

    updating ice analysis

--- ice_inc/adjust-income/export/src/ice-adjust-quintiles.R
+++ ice_inc/adjust-income/export/src/ice-adjust-quintiles.R
@@ -17,20 +17,16 @@ p_load(dplyr,
        here)
 
 # args {{{
-args <- list(input1 = here("ice_inc/import/output/IceCensus.csv")
-             hand = here("ice_inc/import/notes/IceIncVarsDic.txt"))
+args <- list(input1 = here("ice_inc/import/output/IceCensus.csv"),
+             input2 = here("ice_inc/adjust-income/measure-quintiles/output/HI-quintiles.csv"),
+             hand = here("ice_inc/import/notes/IceIncVarsDic.txt"),
+             output = here("ice_inc/adjust-income/export/output/ICE-adjustedInc.csv"))
 
 
 # -- Import ---
-income_data_census <- fread(args$input1)
+income_census <- fread(args$input1)
 quintiles_PUMS <- fread(args$input2)
 desc_vars_census <- read.table(args$hand, sep = "\t", header = TRUE)
-
-
-
-print(quintiles_PUMS)
-print(desc_vars_census)
-
 
 
 
@@ -40,13 +36,8 @@ print(desc_vars_census)
 
 ## Quintile 1 (1st Quintile (0% - 20%), x <= 7808) 
 # 2  B19001_002 hhinc1 household income <$10k
-
-income_data_census <- income_data_census %>% 
-  mutate(hhinc_7808 = ((quintiles_PUMS$upper.limits[1] - 0)/10000) * hhinc1) 
-
 # hhinc_7808 = ((threshold - minimum value) / orig category) * total households in category
 # Measure is based on the proportion and assumes uniform distribution 
-
 
 ## Quintiles 5 ()
 # 10 B19001_010  hhinc9 household income $45k-49,999
@@ -54,20 +45,26 @@ income_data_census <- income_data_census %>%
 # =((Upperlimit in category - category of interest)/intervalo de precio)* total de households en la categoria
 # = (Total households > 45,000) * ((Threshold - Minimum income) / (Maximum income - Minimum income))
 
-income_data_census <- income_data_census %>% 
-  mutate(hhinc_49300 = ((49999-quintiles_PUMS$upper.limits[4])/5000) * hhinc9) # This includes 
-
-income_data_census %>% select(hhinc_49300, hhinc9)
 
 # hhinc_49300 = ((Upperlimit in category - category of interest)/intervalo de precio) * total de households en la categoria
 # Measure is based on the proportion that this income corresponds to the total category of income 
 
 
+
+
+income_adjusted <- income_census %>% 
+  mutate(hhinc_7808 = ((quintiles_PUMS$upper.limits[1] - 0)/10000) * hhinc1,
+         hhinc_49300 = ((49999-quintiles_PUMS$upper.limits[4])/5000) * hhinc9) 
+
+
 ## Conclusion
 # At this point, Q1 (0-7808) is basically measured with the first category after estimating 7808 from 10,0000 income pop
 # Q5 (49300-828000) would be the lower limit of the adjusted category of household income $45k-49,999 
-# and then we must add the other income cats. This cats would be hhinc49300-hhinc16.
+# and then we must add the other income cats. This cats would be hhinc49300 + hhinc10-hhinc16.
 
 
+# -- Output ---
+fwrite(income_adjusted, args$output)
 
+## Final task will be used to create the Quintiles. 
 
--- ice_inc/analysis/src/ice-analysis.R
+++ ice_inc/analysis/src/ice-analysis.R
@@ -3,6 +3,8 @@
 # Date: 26-feb-24
 # ===========================================
 
+# this code need the dataset from 
+# Merged --> with this dataset the analysis can be finished 
 # Analyzing rates of mortality in
 # ICE income quintiles 
 
@@ -16,49 +18,40 @@ p_load(dplyr,
        tidyr)
 
 # args {{{
-args <- list(input = here("ice_inc/import/PUMS_inc/input/psam_h72.csv"),
-             output = here("ice_inc/import/PUMS_inc/output/pumsHIncome.csv"))
+args <- list(input = here("ice_inc/merge/output/iceInc-geo-final.csv"),
+             output = here(""))
 
-# Import ICE data (new task to merge)
-ice_inc<- 
+# Import ICE data (new task to merge)fre
+ice_inc <- fread(args$input)
 
   
-# save the geommetry data separately
-ice_inc_geometry <- ICEraceinc %>% select(GEOID) %>% unique()
+## Conclusion
+# At this point, Q1 (0-7808) is basically measured with the first category after estimating 7808 from 10,0000 income pop
+# Q5 (49300-828000) would be the lower limit of the adjusted category of household income $45k-49,999 
+# and then we must add the other income cats. This cats would be hhinc49300 + hhinc10-hhinc16.
+  
+# Tengo que pensar mejor las cantidades totales de poblacion por tramo censal
+# PORQUE TIENE que reflejar los ajusted de los intervalos 
 
-
+# Verificar porque hhinc_total 
 
 # 1. Calculate ICE 
 # calculate the ICE for racialized economic segregation
 ice_inc <- ice_inc %>% 
   mutate(
-    people_deprived  = 
-      (hhinc_total_1 + hhinc_total_2 + hhinc_total_3 + hhinc_total_4) - 
-      (hhinc_w_1 + hhinc_w_2 + hhinc_w_3 + hhinc_w_4),
-    
-    
-    # sum up the white non-hispanic high income counts
-    people_afluent = 
-      (hhinc_w_5 + hhinc_w_6 + hhinc_w_7 + hhinc_w_8),
-    
-    
-    # calculate the index of concentration at the extremes for economic segregation
-    # (high income vs. low income)
-    
+    people_deprived  = hhinc_7808,
+    people_afluent = (hhinc_49300 + hhinc10 + hhinc11 + hhinc12 + hhinc13 +
+      hhinc14 + hhinc15 + hhinc16),
     ICEinc = 
       (people_deprived - people_afluent) / 
-      hhinc_total. # REVISAR CUAL ES ESTA BAR EN EL IMPORT DE ICE 
-  )
+      hhinc_total) 
 
-# now we can merge our spatial geometry data back in
-ICEraceinc <- ICEraceinc_geometry %>% 
-  left_join(ICEraceinc %>% select(GEOID, ICEraceinc))
 
 
 # 2. Divide ICE measures into Quintiles 
-ice_quintiles <- ice_inc %>%
+ice_inc <- ice_inc %>%
   arrange(ICEinc) %>%  # Ensure data is sorted by ICE
-  mutate(quintile = ntile(ICE_inc, 5))  # Divide into quintiles
+  mutate(ICEquintile = ntile(ICEinc, 5))  # Divide into quintiles
 
 
 
@@ -66,9 +59,9 @@ ice_quintiles <- ice_inc %>%
 # 3. Calculate mortality rates for each group of quintiles of ICE (Q1,Q2,Q3,Q4,Q5)
 
 ice_inc %>%
-  group_by(quintiles) %>% 
-  summarise(child_rip = sum(child_mortality),
-            total_pop = sum(total_pop),
+  group_by(ICEquintile) %>% 
+  summarise(child_rip = sum(RIP),
+            total_pop = sum(pop_total),    # Aqui uso la poblacion del tramo censal
             rate = (child_rip / total_pop) * 1000000)
 
 
